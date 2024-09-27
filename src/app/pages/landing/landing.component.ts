4aa521d1d19413667c038be4426da293d7a2b370
Jérémy Munsch <github@jeremydev.ovh>
2024-09-27 11:56:21 +0200

    programmatic router navigation

--- src/app/pages/landing/landing.component.ts
+++ src/app/pages/landing/landing.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core'
+import { Router } from '@angular/router'
 
 @Component({
   selector: 'app-landing',
@@ -6,5 +7,10 @@ import { Component } from '@angular/core'
   styleUrl: './landing.component.scss',
 })
 export class LandingComponent {
+  constructor(private router: Router) {}
 
+  onContinue(e: MouseEvent): void {
+    e.preventDefault()
+    this.router.navigateByUrl('facesnaps')
+  }
 }
